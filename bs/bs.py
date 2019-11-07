d49f2c186ff4243ee43314e10fcdff421019271d
milan252525 <milan.abraham2@seznam.cz>
2019-11-07 22:47:10 +0100

    added average trophies

--- bs/bs.py
+++ bs/bs.py
@@ -223,11 +223,13 @@ class BrawlStarsCog(commands.Cog):
         embed.set_author(name=f"{club.name} {club.tag}")
         embed.add_field(name="Total Trophies", value= f"<:bstrophy:552558722770141204> {club.trophies}")
         embed.add_field(name="Required Trophies", value= f"{self.get_league_emoji(club.required_trophies)} {club.required_trophies}")
+        embed.add_field(name="Average Trophies", value= f"<:bstrophy:552558722770141204> {club.trophies//len(club.members)}")
         embed.add_field(name="Members", value=f"<:icon_gameroom:553299647729238016> {len(club.members)}/100")
         embed.add_field(name="Status", value= f"<:bslock:552560387279814690> {club.type}")
         for m in club.members:
             if m.role == "president":
-                embed.add_field(name="President", value= f"<:leader:642112580965367837> {self.get_league_emoji(m.trophies)}`{m.trophies}` {m.name}")
+                embed.add_field(name="President", value= f"<:leader:642116605295329290> {self.get_league_emoji(m.trophies)}`{m.trophies}` {m.name}")
+                break
         topm = ""
         for i in range(5):
             try:
@@ -237,7 +239,7 @@ class BrawlStarsCog(commands.Cog):
         worstm = ""
         for i in range(5):
             try:
-                worstm += f"{self.get_league_emoji(club.members[len(club.members)-6+i].trophies)}`{club.members[len(club.members)-6+i].trophies}` {self.remove_codes(club.members[len(club.members)-6+i].name)}\n"
+                worstm += f"{self.get_league_emoji(club.members[len(club.members)-5+i].trophies)}`{club.members[len(club.members)-5+i].trophies}` {self.remove_codes(club.members[len(club.members)-5+i].name)}\n"
             except IndexError:
                 pass
         embed.add_field(name = "Top Members", value = topm, inline = True)
