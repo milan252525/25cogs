d550a79406673f60b41a69c4ec481d88a87f1e43
milan252525 <milan.abraham2@seznam.cz>
2019-11-07 22:52:39 +0100

    some markdown

--- bs/bs.py
+++ bs/bs.py
@@ -221,14 +221,14 @@ class BrawlStarsCog(commands.Cog):
         
         embed=discord.Embed(description=f"```{self.remove_codes(club.description)}```")
         embed.set_author(name=f"{club.name} {club.tag}")
-        embed.add_field(name="Total Trophies", value= f"<:bstrophy:552558722770141204> {club.trophies}")
-        embed.add_field(name="Required Trophies", value= f"{self.get_league_emoji(club.required_trophies)} {club.required_trophies}")
+        embed.add_field(name="Total Trophies", value= f"<:bstrophy:552558722770141204> `{club.trophies}`")
+        embed.add_field(name="Required Trophies", value= f"{self.get_league_emoji(club.required_trophies)} `{club.required_trophies}`")
         embed.add_field(name="Average Trophies", value= f"<:bstrophy:552558722770141204> {club.trophies//len(club.members)}")
-        embed.add_field(name="Members", value=f"<:icon_gameroom:553299647729238016> {len(club.members)}/100")
-        embed.add_field(name="Status", value= f"<:bslock:552560387279814690> {club.type}")
+        embed.add_field(name="Members", value=f"<:icon_gameroom:553299647729238016> **{len(club.members)}**/100")
+        embed.add_field(name="Status", value= f"<:bslock:552560387279814690> {club.type.title()}")
         for m in club.members:
             if m.role == "president":
-                embed.add_field(name="President", value= f"<:leader:642116605295329290> {self.get_league_emoji(m.trophies)}`{m.trophies}` {m.name}")
+                embed.add_field(name="President", value= f"{self.get_league_emoji(m.trophies)}`{m.trophies}` {m.name}", inline = False)
                 break
         topm = ""
         for i in range(5):
