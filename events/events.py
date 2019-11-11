e9485c88a2e9ae227ee805976cf2fd71b8473942
milan252525 <milan.abraham2@seznam.cz>
2019-11-11 20:15:56 +0100

    finish function

--- events/events.py
+++ events/events.py
@@ -11,17 +11,20 @@ class Events(commands.Cog):
         #self.config = Config.get_conf(self, identifier=25993325)
         #default_member = {"bossfight" : {"damage_dealt" : 0, "powercubes" : 0}}
         #self.config.register_member(**default_member)
+        self.BOSS_HP = 5000
+        self.DAMAGE_PER_CUBE = 100
         self.data = {
             "bossfight" : {
                 "active" : False,
                 "channel" : None,
                 "message" : None,
                 "embed" : None,
-                "hp_left" : 5000
+                "hp_left" : self.BOSS_HP
                 }
             }
         self.actions = []
         self.players = {}
+        
 
     def cog_unload(self):
         self.messageupdateloop.stop()
@@ -37,13 +40,15 @@ class Events(commands.Cog):
             elif action[0] == "P":
                 new_actions_str += f"{action[1]} collected a power cube ({action[2]})\n"
         self.actions = []
+        if self.data["bossfight"]["hp_left"] <= 0:
+            await self.finish()
         embed = self.data["bossfight"]["embed"]
-        embed.set_field_at(0, name="HP Left", value=f"{self.data['bossfight']['hp_left']}/5000", inline=False)
+        embed.set_field_at(0, name="HP Left", value=f"{self.data['bossfight']['hp_left']}/{self.BOSS_HP}", inline=False)
         embed.set_field_at(1, name="Action log:", value=new_actions_str)
         await self.data["bossfight"]["message"].edit(embed=embed)
 
 
-    @tasks.loop(seconds=15)
+    @tasks.loop(seconds=30)
     async def randomspawnloop(self):
         rand = randint(0,10)
         if rand < 5:
@@ -52,7 +57,14 @@ class Events(commands.Cog):
     # @tasks.loop(seconds=20)
     # async def randomkillloop(self):
     #     print()
-        
+
+    async def finish(self):
+        embed = self.data["bossfight"]["embed"]
+        embed.set_field_at(0, name="HP Left", value=f"0/{self.BOSS_HP}", inline=False)
+        embed.add_field(name="Action log:", value="Boss was defeated!")
+        self.messageupdateloop.stop()
+        self.randomspawnloop.stop()
+
     async def spawn_cube(self):
         embed = discord.Embed(description="<:powercube:643517745199054855> Power cube spawned!\nPick it up by reacting!", colour=discord.Color.green())
         message = await self.data["bossfight"]["channel"].send(embed=embed)
@@ -64,7 +76,7 @@ class Events(commands.Cog):
                 self.players[user.id] = {"damage" : 0, "power_cubes" : 0}
         self.players[user.id]["power_cubes"] += 1
         await message.edit(embed=discord.Embed(description=f"<:powercube:643517745199054855> {user.mention} collected the power cube!\nCurrent amount: {self.players[user.id]['power_cubes']}", colour=discord.Color.green()))
-        self.actions.append(["P", message.author.mention, self.players[user.id]["power_cubes"]])
+        self.actions.append(["P", user.mention, self.players[user.id]["power_cubes"]])
         await message.delete(delay=3)
 
     @commands.Cog.listener()
@@ -72,7 +84,7 @@ class Events(commands.Cog):
         if not message.author.bot and self.data["bossfight"]["active"] and message.channel == self.data["bossfight"]["channel"]:
             if message.author.id not in self.players.keys():
                 self.players[message.author.id] = {"damage" : 0, "power_cubes" : 0}
-            self.actions.append(["D", message.author.mention, (self.players[message.author.id]["power_cubes"] + 1) * 100])
+            self.actions.append(["D", message.author.mention, (self.players[message.author.id]["power_cubes"] + 1) * self.DAMAGE_PER_CUBE])
             await message.delete(delay=1)
             
         
@@ -84,17 +96,20 @@ class Events(commands.Cog):
 
         embed = discord.Embed(title="BOSS FIGHT", colour=discord.Colour.red())
         embed.set_thumbnail(url="https://i.imgur.com/fo3Tqfd.png")
-        embed.add_field(name="HP Left", value=f"{self.data['bossfight']['hp_left']}/5000", inline=False)
+        embed.add_field(name="HP Left", value=f"{self.data['bossfight']['hp_left']}/{self.BOSS_HP}", inline=False)
         embed.add_field(name="Starting in:", value="5 seconds")
         
         main_message = await channel.send(embed=embed)
         self.data["bossfight"]["message"] = main_message
-        self.data["bossfight"]["embed"] = embed
+        
 
         await sleep(5)
 
         embed.set_field_at(1, name="Action log:", value="Boss Fight started!")
+        embed.set_footer(text="Damage boss by sending messages!")
         await self.data["bossfight"]["message"].edit(embed=embed)
+
+        self.data["bossfight"]["embed"] = embed
 
         self.data["bossfight"]["active"] = True
 
