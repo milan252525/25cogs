64e9d012939388240833b859e14017351c1428d7
Anastasia Tarabrina <anastasiya.tarabrina@ru.axxonsoft.com>
2019-12-05 21:49:45 +0300

    Functionality to sort roles (draft).

--- bs/bs.py
+++ bs/bs.py
@@ -403,12 +403,13 @@ class BrawlStarsCog(commands.Cog):
 
     @commands.guild_only()
     @commands.has_permissions(administrator=True)
+    @commands.command()
     async def sortroles(self, ctx):
+        guest = discord.utils.get(ctx.guild.roles, name='Guest')
+        bot = discord.utils.get(ctx.guild.roles, name='Bots')
         for member in ctx.guild.members:
-            guest = discord.utils.get(ctx.guild.roles, name='Guest')
-            for role in member.roles:
-                if role == guest:
-                    continue
+            if guest in member.roles or bot in member.roles:
+                continue
             tag = await self.config.user(member).tag()
             if tag is None:
                 continue
@@ -428,11 +429,11 @@ class BrawlStarsCog(commands.Cog):
                 if role.name.startswith('LA '):
                     memberrole = role
                     club = role.name.split(':', 1)[0].strip()
-            if player.club is None and memberrole is not None:
+            if (player.club is None or 'LA ' not in player.club.name) and memberrole is not None:
                 await ctx.send(f'{str(member)} has no club but has the role {club}')
-            elif memberrole is None:
+            elif memberrole is None and 'LA ' in player.club.name:
                 await ctx.send(f'{str(member)} is in {player.club.name}, currently has no club roles')
-            elif club not in player.club.name:
+            elif player.club.name not in club and 'LA ' in player.club.name:
                 await ctx.send(f'{str(member)} should be in {club}, currently in {player.club.name}')
 
 
