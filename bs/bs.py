5a58d47c80d3afd933eaffafe905efa9062bd053
Anastasia Tarabrina <anastasiya.tarabrina@ru.axxonsoft.com>
2019-12-08 19:19:12 +0300

    Functionality to sort roles (test run).

--- bs/bs.py
+++ bs/bs.py
@@ -405,7 +405,9 @@ class BrawlStarsCog(commands.Cog):
     @commands.has_permissions(administrator=True)
     @commands.command()
     async def sortroles(self, ctx):
-        guest = discord.utils.get(ctx.guild.roles, name='Guest')
+        labs = discord.utild.get(ctx.guild.roles, id="576028728052809728")
+        guest = discord.utils.get(ctx.guild.roles, id="578260960981286923")
+        msg = ""
         for member in ctx.guild.members:
             if guest in member.roles or member.bot:
                 continue
@@ -428,11 +430,35 @@ class BrawlStarsCog(commands.Cog):
                 if role.name.startswith('LA '):
                     memberrole = role
                     club = role.name.split(':', 1)[0].strip()
-            if (player.club is None or 'LA ' not in player.club.name) and memberrole is not None:
-                await ctx.send(f'{str(member)} has no club but has the role {club}')
-            elif memberrole is None and player.club != None and 'LA ' in player.club.name:
-                await ctx.send(f'{str(member)} is in {player.club.name}, currently has no club roles')
-            elif player.club != None and player.club.name not in club and 'LA ' in player.club.name:
-                await ctx.send(f'{str(member)} should be in {club}, currently in {player.club.name}')
+            if (player.club is None or 'LA ' not in player.club.name) and memberrole is not None: #member -> guest
+                msg += self.removeroleifpresent(member, memberrole)
+                msg += self.removeroleifpresent(member, labs)
+                msg += self.addroleifnotpresent(member, guest)
+            elif memberrole is None and player.club != None and 'LA ' in player.club.name: #guest -> member
+                for r in ctx.guild.roles:
+                    if r.name.startswith(player.club.name):
+                        msg += self.addroleifnotpresent(member, r)
+                    else:
+                        msg += f"Role for the club {player.club.name} not found.\n"
+                        continue
+                msg += self.addroleifnotpresent(member, labs)
+                msg += self.removeroleifpresent(member, guest)
+            elif player.club is not None and player.club.name not in club and 'LA ' in player.club.name and memberrole is not None: #one club -> another club
+                for r in ctx.guild.roles:
+                    if r.name.startswith(player.club.name):
+                        msg += self.addroleifnotpresent(member, r)
+                    else:
+                        msg += f"Role for the club {player.club.name} not found.\n"
+                        continue
+                msg += self.removeroleifpresent(member, memberrole)
+        await ctx.send(msg)
 
+    async def removeroleifpresent(self, member: discord.Member, role: discord.Role):
+        if role in member.roles:
+            await member.remove_roles(role)
+        return f"Removed {str(role)} from {str(member)}\n"
 
+    async def addroleifnotpresent(self, member: discord.Member, role: discord.Role):
+        if role not in member.roles:
+            await member.add_roles(role)
+        return f"Added {str(role)} to {str(member)}\n"
