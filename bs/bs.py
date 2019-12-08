8ebdb64fb489dba883198374403b885819768e6f
Anastasia Tarabrina <anastasiya.tarabrina@ru.axxonsoft.com>
2019-12-08 23:42:08 +0300

    player.club > player.club.tag

--- bs/bs.py
+++ bs/bs.py
@@ -426,6 +426,8 @@ class BrawlStarsCog(commands.Cog):
         guest = discord.utils.get(ch.guild.roles, id=578260960981286923)
         newcomer = discord.utils.get(ch.guild.roles, id=534461445656543255)
         brawlstars = discord.utils.get(ch.guild.roles, id=576002604740378629)
+        vp = discord.utils.get(ch.guild.roles, id=536993652648574976)
+        pres = discord.utils.get(ch.guild.roles, id=536993632918568991)
         for member in ch.guild.members:
             if guest in member.roles or member.bot:
                 continue
@@ -460,47 +462,47 @@ class BrawlStarsCog(commands.Cog):
                 club = memberrole.name.split(':', 1)[0].strip()
 
             if newcomer in member.roles: #newcomer -> member
-                if player.club is None or 'LA ' not in player.club.name:
+                if player.club is None or 'LA ' not in player.club.tag:
                     msg += await self.removeroleifpresent(member, newcomer)
                     msg += await self.addroleifnotpresent(member, brawlstars)
                     msg += await self.addroleifnotpresent(member, guest)
                     await ch.send(embed=discord.Embed(colour=discord.Colour.blue(), description=msg))
-                elif player.club is not None and 'LA ' in player.club.name:
+                elif player.club is not None and 'LA ' in player.club.tag:
                     msg += await self.removeroleifpresent(member, newcomer)
                     msg += await self.addroleifnotpresent(member, brawlstars)
                     msg += await self.addroleifnotpresent(member, labs)
                     rolefound = False
                     for r in ch.guild.roles:
-                        if r.name.startswith(player.club.name):
+                        if r.name.startswith(player.club.tag):
                             rolefound = True
                             msg += await self.addroleifnotpresent(member, r)
                     if not rolefound:
-                        msg += f"Role for the club **{player.club.name}** not found.\n"
+                        msg += f"Role for the club **{player.club.tag}** not found.\n"
                     await ch.send(embed=discord.Embed(colour=discord.Colour.blue(), description=msg))
-            elif (player.club is None or 'LA ' not in player.club.name) and memberrole is not None: #member -> guest
+            elif (player.club is None or 'LA ' not in player.club.tag) and memberrole is not None: #member -> guest
                 msg += await self.removeroleifpresent(member, memberrole)
                 msg += await self.removeroleifpresent(member, labs)
                 msg += await self.addroleifnotpresent(member, guest)
                 await ch.send(embed=discord.Embed(colour=discord.Colour.blue(), description=msg))
-            elif memberrole is None and player.club != None and 'LA ' in player.club.name: #guest -> member
+            elif memberrole is None and player.club != None and 'LA ' in player.club.tag: #guest -> member
                 rolefound = False
                 for r in ch.guild.roles:
-                    if r.name.startswith(player.club.name):
+                    if r.name.startswith(player.club.tag):
                         rolefound = True
                         msg += await self.addroleifnotpresent(member, r)
                 if not rolefound:
-                    msg += f"Role for the club {player.club.name} not found."
+                    msg += f"Role for the club {player.club.tag} not found."
                 msg += await self.addroleifnotpresent(member, labs)
                 msg += await self.removeroleifpresent(member, guest)
                 await ch.send(embed=discord.Embed(colour=discord.Colour.blue(), description=msg))
-            elif player.club is not None and player.club.name not in club and 'LA ' in player.club.name and memberrole is not None: #one club -> another club
+            elif player.club is not None and player.club.tag not in club and 'LA ' in player.club.tag and memberrole is not None: #one club -> another club
                 rolefound = False
                 for r in ch.guild.roles:
-                    if r.name.startswith(player.club.name):
+                    if r.name.startswith(player.club.tag):
                         rolefound = True
                         msg += await self.addroleifnotpresent(member, r)
                 if not rolefound:
-                    msg += f"Role for the club {player.club.name} not found."
+                    msg += f"Role for the club {player.club.tag} not found."
                 msg += await self.removeroleifpresent(member, memberrole)
                 await ch.send(embed=discord.Embed(colour=discord.Colour.blue(), description=msg))
         await ch.send(embed=discord.Embed(colour=discord.Colour.green(), description="**Finished.**"))
