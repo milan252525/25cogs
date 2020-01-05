0a86584e1541b2690b63fe00356b3edb6f65b13d
Anastasia Tarabrina <anastasiya.tarabrina@ru.axxonsoft.com>
2020-01-05 23:59:31 +0300

    announcements, actual ids

--- tools/tools.py
+++ tools/tools.py
@@ -274,7 +274,7 @@ class Tools(commands.Cog):
     @commands.has_permissions(administrator=True)
     @commands.command()
     async def announcement(self, ctx, *, message):
-        guilds = dict([(465641254580125696, 663418911378898954), (663416919646535695, 663418966475145277)])
+        guilds = dict([(465641254580125696, 663418911378898954), (663416919646535695, 663418966475145277), (440960893916807188, 538380432748838912), (401883208511389716, 402131630497464340)])
         for key in guilds:
             guild = self.bot.get_guild(key)
             checkmessage = await ctx.send(f"Do you want to send an announcement to **{guild.name}**?")
