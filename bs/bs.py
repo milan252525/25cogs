58950ca7fdc2c014475a623695f7675d7af07e04
Anastasia Tarabrina <anastasiya.tarabrina@ru.axxonsoft.com>
2020-01-08 01:59:46 +0300

    brawler emotes

--- bs/bs.py
+++ bs/bs.py
@@ -65,7 +65,69 @@ class BrawlStarsCog(commands.Cog):
 
     def get_brawler_emoji(self, name : str):
         if name == "SHELLY":
-            return "<:shelly:664232553422651393>"
+            return "<:shelly:664235199076237323>"
+        elif name == "TICK":
+            return "<:tick:664235450889928744>"
+        elif name == "TARA":
+            return "<:tara:664236127015796764>"
+        elif name == "SPIKE":
+            return "<:spike:664235867748958249>"
+        elif name == "SANDY":
+            return "<:sandy:664235310573420544>"
+        elif name == "ROSA":
+            return "<:rosa:664235409722834954>"
+        elif name == "RICO":
+            return "<:rico:664235890171707393>"
+        elif name == "EL PRIMO":
+            return "<:primo:664235742758830135>"
+        elif name == "POCO":
+            return "<:poco:664235668393689099>"
+        elif name == "PIPER":
+            return "<:piper:664235622998867971>"
+        elif name == "PENNY":
+            return "<:penny:664235535094644737>"
+        elif name == "PAM":
+            return "<:pam:664235599804235786>"
+        elif name == "NITA":
+            return "<:nita:664235795959513088>"
+        elif name == "MORTIS":
+            return "<:mortis:664235717693800468>"
+        elif name == "MAX":
+            return "<:max:664235224762155073>"
+        elif name == "LEON":
+            return "<:leon:664235430530514964>"
+        elif name == "JESSIE":
+            return "<:jessie:664235816339636244>"
+        elif name == "GENE":
+            return "<:gene:664235476084981795>"
+        elif name == "FRANK":
+            return "<:frank:664235513242320922>"
+        elif name == "EMZ":
+            return "<:emz:664235245956235287>"
+        elif name == "DYNAMIKE":
+            return "<:dynamike:664235766620094464>"
+        elif name == "DARRYL":
+            return "<:darryl:664235555877290008>"
+        elif name == "CROW":
+            return "<:crow:664235693064716291>"
+        elif name == "COLT":
+            return "<:colt:664235956202766346>"
+        elif name == "CARL":
+            return "<:carl:664235388537274369>"
+        elif name == "BULL":
+            return "<:bull:664235934006378509>"
+        elif name == "BROCK":
+            return "<:brock:664235912150122499>"
+        elif name == "BO":
+            return "<:bo:664235645287530528>"
+        elif name == "BIBI":
+            return "<:bibi:664235367615954964>"
+        elif name == "BEA":
+            return "<:bea:664235276758941701>"
+        elif name == "BARLEY":
+            return "<:barley:664235839316033536>"
+        elif name == "8-BIT":
+            return "<:8bit:664235332522213418>"
         
     def remove_codes(self, text : str):
         toremove = ["</c>", "<c1>", "<c2>", "<c3>", "<c4>", "<c5>", "<c6>", "<c7>", "<c8>", "<c9>", "<c0>"]
