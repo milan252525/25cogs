43a324235f75712a265edca271b9e371d23ac9c3
Anastasia Tarabrina <anastasiya.tarabrina@ru.axxonsoft.com>
2020-01-08 02:17:00 +0300

    brawlers fix

--- bs/bs.py
+++ bs/bs.py
@@ -308,9 +308,14 @@ class BrawlStarsCog(commands.Cog):
         embed.set_author(name=f"{player.name} {player.raw_data['tag']}", icon_url="https://i.imgur.com/ZwIP41S.png")
 
         brawlers = ""
+        messages = []
         for brawler in player.raw_data['brawlers']:
+            if len(brawlers) > 1999:
+                messages.append(brawlers)
+                brawlers = ""
             brawlers += f"{self.get_brawler_emoji(brawler.get('name'))} {brawler.get('name').lower().capitalize()}: {brawler.get('trophies')}\n"
-        embed.add_field(name="**Brawlers:**", value=brawlers)
+        for m in messages:
+            embed.add_field(name="**Brawlers:**", value=m)
         await ctx.send(embed=embed)
 
     @commands.command()
