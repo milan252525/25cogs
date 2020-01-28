b23f962787e851f9842dc6eeb20908f8e5cb75d0
Anastasia Tarabrina <anastasiya.tarabrina@ru.axxonsoft.com>
2020-01-29 01:14:17 +0300

    attempt to fix role system

--- bs/bs.py
+++ bs/bs.py
@@ -706,7 +706,7 @@ class BrawlStarsCog(commands.Cog):
 
             if player_in_club and "LA " in player.club.name:
                 for role in ch.guild.roles:
-                    if sub(r'[^\x00-\x7f]',r'', role.name).strip() == player.club.name.replace("™️", ""):
+                    if sub(r'[^\x00-\x7f]',r'', role.name).strip() == player.club.name.replace("™️", "").strip():
                         member_role_expected = role
                         break
                 if member_role_expected is None:
