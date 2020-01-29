c61e59d0c8f350aebf1d9ad15a2afec37ed28f97
Anastasia Tarabrina <anastasiya.tarabrina@ru.axxonsoft.com>
2020-01-29 18:58:27 +0300

    sortroles fix

--- bs/bs.py
+++ bs/bs.py
@@ -694,6 +694,10 @@ class BrawlStarsCog(commands.Cog):
                 for role in member_roles[1:]:
                     await self.removeroleifpresent(member, role)
 
+            if len(member_roles) == 1:
+                member_role = member_roles[0]
+                await self.removeroleifpresent(member, role)
+
             if not player_in_club:
                 msg += await self.removeroleifpresent(member, labs, vp, pres, newcomer)
                 msg += await self.addroleifnotpresent(member, guest)
