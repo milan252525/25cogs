1d0352f3721eda87829fc138d4d6e1b22c2b6023
Anastasia Tarabrina <anastasiya.tarabrina@ru.axxonsoft.com>
2020-01-29 03:57:08 +0300

    word chain support

--- tools/tools.py
+++ tools/tools.py
@@ -50,7 +50,7 @@ class Tools(commands.Cog):
                 word = msg.content
                 history = await msg.channel.history(limit=2).flatten()
                 if word[0].lower() != history[1].content[-1:].lower():
-                    await msg.channel.send(f"Write a word that starts with {history[1].content[-1:].lower()}, please.", delete_after=2)
+                    await msg.channel.send(f"Write a word that starts with **{history[1].content[-1:].capitalize()}**, please.", delete_after=2)
                     return await msg.delete()
                 if msg.author == history[1].author:
                     await msg.channel.send(f"Don't write two words consecutively.", delete_after=2)
