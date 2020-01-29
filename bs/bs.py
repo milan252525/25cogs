57ae6ca787308fa7095d4dd28d4814e6650c4e8f
Anastasia Tarabrina <anastasiya.tarabrina@ru.axxonsoft.com>
2020-01-29 14:42:13 +0300

    bd tm support

--- bs/bs.py
+++ bs/bs.py
@@ -706,12 +706,12 @@ class BrawlStarsCog(commands.Cog):
 
             if player_in_club and "LA " in player.club.name:
                 for role in ch.guild.roles:
-                    if sub(r'[^\x00-\x7f]',r'', role.name).strip() == player.club.name.replace("™️", "").strip():
+                    if sub(r'[^\x00-\x7f]',r'', role.name).strip() == sub(r'[^\x00-\x7f]',r'', player.club.name).strip():
                         member_role_expected = role
                         break
                 if member_role_expected is None:
                     bd = discord.utils.get(ch.guild.roles, id=664243304778694663)
-                    namerole = player.club.name.replace("™️", "").strip()
+                    namerole = player.club.name.replace("™️️", "").strip()
                     nameclub = sub(r'[^\x00-\x7f]',r'', bd.name).strip()
                     await ch.send(embed=discord.Embed(colour=discord.Colour.blue(), description=f"Role for the club {player.club.name} not found. Club: {namerole}, input: {nameclub}"))
                     continue
