c92980f6a23a8a69bf9cb31a7b63894d136279bf
Anastasia Tarabrina <anastasiya.tarabrina@ru.axxonsoft.com>
2020-02-06 22:28:43 +0300

    bs user by tag

--- bs/bs.py
+++ bs/bs.py
@@ -1051,8 +1051,9 @@ class BrawlStarsCog(commands.Cog):
         if tag.startswith("#"):
             tag = tag.strip('#')
 
-        for user in (await self.config.all_users()).values():
-            await ctx.send(user)
-            return
+        for user in (await self.config.all_users()):
+            if user.value().get("tag") == tag:
+                await ctx.send(user.key())
+                return
 
         await ctx.send(f"This tag is either not saved or invalid.")
