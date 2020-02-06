326eb4d484b139efe7806dc8411aba79e0cf8237
Anastasia Tarabrina <anastasiya.tarabrina@ru.axxonsoft.com>
2020-02-07 00:36:46 +0300

    statistics cog

--- statistics/statistics.py
+++ statistics/statistics.py
@@ -93,12 +93,12 @@ class Statistics(commands.Cog):
                 club = await self.ofcbsapi.get_club(tag)
                 totalmembers = totalmembers + len(club.members)
                 for member in club.members:
-                    members.append(member)
+                    members.append(member.trophies)
             for mem in members:
-                totaltrophies = totaltrophies + mem.trophies
+                totaltrophies = totaltrophies + mem
             members.sort(reverse=True)
             for i in range(len(members) - (len(members)//10), len(members)):
-                lower10 = lower10 + members[i].trophies
+                lower10 = lower10 + members[i]
             averagetrophies = totaltrophies//totalmembers
             embed = discord.Embed(color=discord.Colour.gold(), title="All clubs:")
             embed.add_field(name="Total trophies:", value=totaltrophies, inline=False)
