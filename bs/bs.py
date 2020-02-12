ee07b2459b72fdd72f71e317877de06a1aa300f2
milan_25 <milan.abraham2@seznam.cz>
2020-02-12 22:38:46 +0100

    loop fix

--- bs/bs.py
+++ bs/bs.py
@@ -680,6 +680,8 @@ class BrawlStarsCog(commands.Cog):
                 for role in member_roles:
                     if sub(r'[^\x00-\x7f]', r'', role.name).strip() != sub(r'[^\x00-\x7f]', r'', player.club.name).strip():
                         msg += await self.removeroleifpresent(member, role)
+       
+            member_role = member_roles[0]
 
             if not player_in_club:
                 msg += await self.removeroleifpresent(member, labs, vp, pres, newcomer)
