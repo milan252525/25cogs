28fc59a8c544cc462cbddd27cfbc03c8ebc4945d
milan_25 <milan.abraham2@seznam.cz>
2020-02-16 20:46:28 +0100

    time fix

--- tools/tools.py
+++ tools/tools.py
@@ -28,9 +28,9 @@ class Tools(commands.Cog):
     @commands.Cog.listener()
     async def on_member_remove(self, member):
         if member.guild.id not in self.leave_counter:
-            self.leave_counter[member.guild.id] = [int(time.now())]
+            self.leave_counter[member.guild.id] = [int(time())]
         else:
-            self.leave_counter[member.guild.id].append(int(time.now()))
+            self.leave_counter[member.guild.id].append(int(time()))
         if len(self.leave_counter[member.guild.id]) > 4:
             if self.leave_counter[member.guild.id][-1] - self.leave_counter[member.guild.id][len(-4)] < 300:
                 await self.bot.get_user(230947675837562880).send(f"Members in {member.guild.name} disappearing too fast!")
