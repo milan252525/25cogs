7de97cb7feaa0542816e180a5c595074c0f10305
Anastasia Tarabrina <anastasiya.tarabrina@ru.axxonsoft.com>
2020-02-16 03:08:22 +0300

    memberBS command

--- bs/bs.py
+++ bs/bs.py
@@ -1139,7 +1139,7 @@ class BrawlStarsCog(commands.Cog):
             if tag is None:
                 msg += f"DC tag: {str(member)}; no BS account saved\n"
             elif player_in_club:
-                for mem in club.members:
+                for mem in player.club.members:
                     if mem.tag == player.tag:
                         msg += f"DC tag: {str(member)}; IGN: {player.name}; Club: {player.club.name}; Role: {mem.role}\n"
             elif not player_in_club:
