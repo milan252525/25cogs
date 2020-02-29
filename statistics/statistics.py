38b070f26bb746e8f76c408277830ffcd41cf5a6
Anastasia Tarabrina <anastasiya.tarabrina@ru.axxonsoft.com>
2020-02-29 13:50:38 +0300

    trophy lb

--- statistics/statistics.py
+++ statistics/statistics.py
@@ -7,65 +7,13 @@ class Statistics(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-        self.config = Config.get_conf(self, identifier=424242)
-        default_guild = {"tags": {}}
-        self.config.register_guild(**default_guild)
+        self.bsconfig = Config.get_conf(None, identifier=5245652, cog_name="BrawlStarsCog")
 
     async def initialize(self):
         ofcbsapikey = await self.bot.get_shared_api_tokens("ofcbsapi")
         if ofcbsapikey["api_key"] is None:
             raise ValueError("The Official Brawl Stars API key has not been set.")
         self.ofcbsapi = brawlstats.OfficialAPI(ofcbsapikey["api_key"], is_async=True)
-
-    @commands.command()
-    @commands.guild_only()
-    async def addclub(self, ctx, key : str, tag : str):
-        await ctx.trigger_typing()
-
-        tag = tag.lower().replace('O', '0')
-        if tag.startswith("#"):
-            tag = tag.strip('#')
-
-        try:
-            club = await self.ofcbsapi.get_club(tag)
-            result = {
-                "name": key,
-                "tag": tag
-            }
-            key = key.lower()
-            await self.config.guild(ctx.guild).tags.set_raw(key, value=result)
-            await ctx.send(embed=discord.Embed(description=f"Club {club.name} successfully added.", color=discord.Colour.blue()))
-
-        except brawlstats.errors.NotFoundError:
-            await ctx.send(embed=discord.Embed(description="No club with this tag found.", color=discord.Colour.red()))
-
-        except brawlstats.errors.RequestError as e:
-            await ctx.send(embed=discord.Embed(f"BS API is offline, please try again later! ({str(e)})", color=discord.Colour.red()))
-
-        except Exception as e:
-            await ctx.send(f"**Something went wrong, please send a personal message to LA Modmail bot or try again!**** ({str(e)})")
-
-    @commands.command()
-    @commands.guild_only()
-    async def removeclub(self, ctx, key : str):
-        await ctx.trigger_typing()
-
-        key = key.lower()
-
-        try:
-            await self.config.guild(ctx.guild).tags.clear_raw(key)
-            await ctx.send(embed=discord.Embed(description=f"Club was successfully removed.", color=discord.Colour.blue()))
-        except KeyError:
-            await ctx.send(embed=discord.Embed(description=f"{tag} isn't saved in this server.", color=discord.Colour.red()))
-
-    @commands.is_owner()
-    @commands.command()
-    async def membersinfo(self, ctx):
-        f = open("members.txt", "a+")
-        f.write("test")
-        await ctx.send(file=discord.File(f))
-        f.close()
-
 
     @commands.is_owner()
     @commands.command()
@@ -105,4 +53,35 @@ class Statistics(commands.Cog):
             embed.add_field(name="Lower 10% average:", value=lower10 // (len(club.members)//10), inline=False)
             await ctx.send(embed=embed)
 
-
+    @command.commands()
+    async def trophylb(self, ctx, key:str=None):
+        if key is None:
+            trophies = []
+            for key in (await self.config.guild(ctx.guild).tags()).keys():
+                tag = await self.config.guild(ctx.guild).tags.get_raw(key, "tag")
+                club = await self.ofcbsapi.get_club(tag)
+                for member in club.members:
+                    pair = []
+                    pair.append(member.name)
+                    pair.append(member.trophies)
+                    trophies.append(pair)
+            trophies = sorted(trophies, key=lambda x: x[1], reverse=True)
+            msg = ""
+            i = 1
+            for trophy in trophies:
+                if trophy == trophies[20]:
+                    break
+                msg += f"{i}: {trophy[0]} {trophy[1]}\n"
+                i = i + 1
+            await ctx.send(embed=discord.Embed(color=discord.Colour.gold(), title=f"{club.name} leaderboard:", description=msg))
+        elif key is not None:
+            msg = ""
+            tag = await self.config.guild(ctx.guild).tags.get_raw(key, "tag")
+            club = await self.ofcbsapi.get_club(tag)
+            i = 1
+            for member in club.members:
+                if member == club.members[20]:
+                    break
+                msg += f"{i}: {trophy[0]} {trophy[1]}\n"
+                i = i + 1
+            await ctx.send(embed=discord.Embed(color=discord.Colour.gold(), title=f"{club.name} leaderboard:", description=msg))
