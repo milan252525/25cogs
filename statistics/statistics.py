98550975864bf4e6955955ae07d6c7fe5fcbbc6e
Anastasia Tarabrina <anastasiya.tarabrina@ru.axxonsoft.com>
2020-04-30 16:33:10 +0300

    fixes

--- statistics/statistics.py
+++ statistics/statistics.py
@@ -109,97 +109,100 @@ class Statistics(commands.Cog):
     @tasks.loop(minutes=60)
     async def lbrenewallabs(self):
         channel = self.bot.get_channel(689889206230974473)
-        message = await channel.fetch_message(691747287814373428)
-        message2 = await channel.fetch_message(691747288988647534)
-        message3 = await channel.fetch_message(691747290398064741)
-        message4 = await channel.fetch_message(691747291119222875)
-        message5 = await channel.fetch_message(691747292511993866)
-        trophies = []
-        for key in (await self.bsconfig.guild(message.guild).clubs()).keys():
-            tag = await self.bsconfig.guild(message.guild).clubs.get_raw(key, "tag")
-            club = await self.ofcbsapi.get_club(tag)
-            for member in club.members:
-                pair = []
-                pair.append(member.name)
-                pair.append(member.trophies)
-                pair.append(club.name)
-                trophies.append(pair)
-        trophies = sorted(trophies, key=lambda x: x[1], reverse=True)
-        messages = []
-        msg = ""
-        i = 1
-        for trophy in trophies:
-            if trophy == trophies[20] or trophy == trophies[40] or trophy == trophies[60] or trophy == trophies[80] or trophy == trophies[100]:
-                messages.append(msg)
-                msg = ""
-            msg += f"{i}. <:bstrophy:552558722770141204> {trophy[1]} **{trophy[0]}**({trophy[2]})\n"
-            i = i + 1
-        for m in messages:
-            if m == messages[0]:
-                embed = discord.Embed(color=discord.Colour.gold(), title=f"{message.guild.name} leaderboard:", description=m)
-                await message.edit(embed=embed)
-            elif m == messages[1]:
-                embed = discord.Embed(color=discord.Colour.gold(), description=m)
-                await message2.edit(embed=embed)
-            elif m == messages[2]:
-                embed = discord.Embed(color=discord.Colour.gold(), description=m)
-                await message3.edit(embed=embed)
-            elif m == messages[3]:
-                embed = discord.Embed(color=discord.Colour.gold(), description=m)
-                await message4.edit(embed=embed)
-            elif m == messages[4]:
-                embed = discord.Embed(color=discord.Colour.gold(), description=m)
-                await message5.edit(embed=embed)
+        if channel is not None:
+            message = await channel.fetch_message(691747287814373428)
+            message2 = await channel.fetch_message(691747288988647534)
+            message3 = await channel.fetch_message(691747290398064741)
+            message4 = await channel.fetch_message(691747291119222875)
+            message5 = await channel.fetch_message(691747292511993866)
+            trophies = []
+            for key in (await self.bsconfig.guild(message.guild).clubs()).keys():
+                tag = await self.bsconfig.guild(message.guild).clubs.get_raw(key, "tag")
+                club = await self.ofcbsapi.get_club(tag)
+                for member in club.members:
+                    pair = []
+                    pair.append(member.name)
+                    pair.append(member.trophies)
+                    pair.append(club.name)
+                    trophies.append(pair)
+            trophies = sorted(trophies, key=lambda x: x[1], reverse=True)
+            messages = []
+            msg = ""
+            i = 1
+            for trophy in trophies:
+                if trophy == trophies[20] or trophy == trophies[40] or trophy == trophies[60] or trophy == trophies[80] or trophy == trophies[100]:
+                    messages.append(msg)
+                    msg = ""
+                msg += f"{i}. <:bstrophy:552558722770141204> {trophy[1]} **{trophy[0]}**({trophy[2]})\n"
+                i = i + 1
+            for m in messages:
+                if m == messages[0]:
+                    embed = discord.Embed(color=discord.Colour.gold(), title=f"{message.guild.name} leaderboard:", description=m)
+                    await message.edit(embed=embed)
+                elif m == messages[1]:
+                    embed = discord.Embed(color=discord.Colour.gold(), description=m)
+                    await message2.edit(embed=embed)
+                elif m == messages[2]:
+                    embed = discord.Embed(color=discord.Colour.gold(), description=m)
+                    await message3.edit(embed=embed)
+                elif m == messages[3]:
+                    embed = discord.Embed(color=discord.Colour.gold(), description=m)
+                    await message4.edit(embed=embed)
+                elif m == messages[4]:
+                    embed = discord.Embed(color=discord.Colour.gold(), description=m)
+                    await message5.edit(embed=embed)
 
     @tasks.loop(minutes=70)
     async def lbrenewalasia(self):
         channel = self.bot.get_channel(690133321245917204)
-        message = await channel.fetch_message(690133472366428160)
-        trophies = []
-        for key in (await self.bsconfig.guild(message.guild).clubs()).keys():
-            tag = await self.bsconfig.guild(message.guild).clubs.get_raw(key, "tag")
-            club = await self.ofcbsapi.get_club(tag)
-            for member in club.members:
-                pair = []
-                pair.append(member.name)
-                pair.append(member.trophies)
-                pair.append(club.name)
-                trophies.append(pair)
-        trophies = sorted(trophies, key=lambda x: x[1], reverse=True)
-        msg = ""
-        i = 1
-        for trophy in trophies:
-            if trophy == trophies[20]:
-                break
-            msg += f"{i}. <:bstrophy:552558722770141204> {trophy[1]} **{trophy[0]}**({trophy[2]})\n"
-            i = i + 1
-        embed = discord.Embed(color=discord.Colour.gold(), title=f"{message.guild.name} leaderboard:", description=msg)
-        await message.edit(embed=embed)
+        if channel is not None:
+            message = await channel.fetch_message(690133472366428160)
+            trophies = []
+            for key in (await self.bsconfig.guild(message.guild).clubs()).keys():
+                tag = await self.bsconfig.guild(message.guild).clubs.get_raw(key, "tag")
+                club = await self.ofcbsapi.get_club(tag)
+                for member in club.members:
+                    pair = []
+                    pair.append(member.name)
+                    pair.append(member.trophies)
+                    pair.append(club.name)
+                    trophies.append(pair)
+            trophies = sorted(trophies, key=lambda x: x[1], reverse=True)
+            msg = ""
+            i = 1
+            for trophy in trophies:
+                if trophy == trophies[20]:
+                    break
+                msg += f"{i}. <:bstrophy:552558722770141204> {trophy[1]} **{trophy[0]}**({trophy[2]})\n"
+                i = i + 1
+            embed = discord.Embed(color=discord.Colour.gold(), title=f"{message.guild.name} leaderboard:", description=msg)
+            await message.edit(embed=embed)
 
     @tasks.loop(minutes=80)
     async def lbrenewalbd(self):
         channel = self.bot.get_channel(691278145185251328)
-        message = await channel.fetch_message(691296279157801050)
-        trophies = []
-        for key in (await self.bsconfig.guild(message.guild).clubs()).keys():
-            tag = await self.bsconfig.guild(message.guild).clubs.get_raw(key, "tag")
-            club = await self.ofcbsapi.get_club(tag)
-            for member in club.members:
-                pair = []
-                pair.append(member.name)
-                pair.append(member.trophies)
-                pair.append(club.name)
-                trophies.append(pair)
-        trophies = sorted(trophies, key=lambda x: x[1], reverse=True)
-        msg = ""
-        i = 1
-        for trophy in trophies:
-            if trophy == trophies[20]:
-                break
-            msg += f"{i}. <:bstrophy:552558722770141204> {trophy[1]} **{trophy[0]}**({trophy[2]})\n"
-            i = i + 1
-        embed = discord.Embed(color=discord.Colour.gold(), title=f"{message.guild.name} leaderboard:", description=msg)
-        await message.edit(embed=embed)
+        if channel is not None:
+            message = await channel.fetch_message(691296279157801050)
+            trophies = []
+            for key in (await self.bsconfig.guild(message.guild).clubs()).keys():
+                tag = await self.bsconfig.guild(message.guild).clubs.get_raw(key, "tag")
+                club = await self.ofcbsapi.get_club(tag)
+                for member in club.members:
+                    pair = []
+                    pair.append(member.name)
+                    pair.append(member.trophies)
+                    pair.append(club.name)
+                    trophies.append(pair)
+            trophies = sorted(trophies, key=lambda x: x[1], reverse=True)
+            msg = ""
+            i = 1
+            for trophy in trophies:
+                if trophy == trophies[20]:
+                    break
+                msg += f"{i}. <:bstrophy:552558722770141204> {trophy[1]} **{trophy[0]}**({trophy[2]})\n"
+                i = i + 1
+            embed = discord.Embed(color=discord.Colour.gold(), title=f"{message.guild.name} leaderboard:", description=msg)
+            await message.edit(embed=embed)
 
     @commands.command()
     @commands.guild_only()
