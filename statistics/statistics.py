5ae6ac5f9365102c8b718699c0c967cf64d5403c
Anastasia Tarabrina <anastasiya.tarabrina@ru.axxonsoft.com>
2020-05-16 01:37:57 +0300

    lb spain

--- statistics/statistics.py
+++ statistics/statistics.py
@@ -14,11 +14,13 @@ class Statistics(commands.Cog):
         self.lbrenewallabs.start()
         self.lbrenewalasia.start()
         self.lbrenewalbd.start()
+        self.lbrenewalspain.start()
 
     def cog_unload(self):
         self.lbrenewallabs.cancel()
         self.lbrenewalasia.cancel()
         self.lbrenewalbd.cancel()
+        self.lbrenewalspain.cancel()
 
     async def initialize(self):
         crapikey = await self.bot.get_shared_api_tokens("crapi")
@@ -240,3 +242,51 @@ class Statistics(commands.Cog):
                 i = i + 1
             embed = discord.Embed(color=discord.Colour.gold(), title=f"{clan.name} leaderboard:", description=msg)
             await ctx.send(embed=embed)
+
+    @tasks.loop(minutes=90)
+    async def lbrenewalspain(self):
+        channel = self.bot.get_channel(691413764867751986)
+        if channel is not None:
+            message = await channel.fetch_message(710980939492229181)
+            message2 = await channel.fetch_message(710984125992927334)
+            message3 = await channel.fetch_message(710984138265460836)
+            message4 = await channel.fetch_message(710984144749985923)
+            message5 = await channel.fetch_message(710984150747840554)
+            trophies = []
+            for key in (await self.bsconfig.guild(message.guild).clubs()).keys():
+                tag = await self.bsconfig.guild(message.guild).clubs.get_raw(key, "tag")
+                club = await self.ofcbsapi.get_club(tag)
+                for member in club.members:
+                    pair = []
+                    pair.append(member.name)
+                    pair.append(member.trophies)
+                    pair.append(club.name)
+                    trophies.append(pair)
+            trophies = sorted(trophies, key=lambda x: x[1], reverse=True)
+            messages = []
+            msg = ""
+            i = 1
+            for trophy in trophies:
+                if trophy == trophies[20] or trophy == trophies[40] or trophy == trophies[60] or trophy == trophies[
+                    80] or trophy == trophies[100]:
+                    messages.append(msg)
+                    msg = ""
+                msg += f"{i}. <:bstrophy:552558722770141204> {trophy[1]} **{trophy[0]}**({trophy[2]})\n"
+                i = i + 1
+            for m in messages:
+                if m == messages[0]:
+                    embed = discord.Embed(color=discord.Colour.gold(), title=f"{message.guild.name} leaderboard:",
+                                          description=m)
+                    await message.edit(embed=embed)
+                elif m == messages[1]:
+                    embed = discord.Embed(color=discord.Colour.gold(), description=m)
+                    await message2.edit(embed=embed)
+                elif m == messages[2]:
+                    embed = discord.Embed(color=discord.Colour.gold(), description=m)
+                    await message3.edit(embed=embed)
+                elif m == messages[3]:
+                    embed = discord.Embed(color=discord.Colour.gold(), description=m)
+                    await message4.edit(embed=embed)
+                elif m == messages[4]:
+                    embed = discord.Embed(color=discord.Colour.gold(), description=m)
+                    await message5.edit(embed=embed)
