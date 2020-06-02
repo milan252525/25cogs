8ec8722c4105914c259b0ded74265a83ac3fa57c
Anastasia Tarabrina <anastasiya.tarabrina@ru.axxonsoft.com>
2020-06-02 22:48:34 +0300

    test

--- statistics/statistics.py
+++ statistics/statistics.py
@@ -432,9 +432,3 @@ class Statistics(commands.Cog):
             await ctx.send(embed=goodEmbed(f"{ign} was successfully removed from this server's blacklist!"))
         except KeyError:
             await ctx.send(embed=badEmbed(f"{ign} isn't blacklisted in this server!"))
-
-    @commands.command()
-    @commands.has_permissions(administrator=True)
-    async def imadumbfuk(self, ctx):
-        player = await self.ofcbsapi.get_player("89l8cv8ur")
-        await ctx.send(type(player))
