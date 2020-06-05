9583ef2e7233accccae6fbc340c9af571c68cdb8
milan_25 <you@example.com>
2020-06-05 21:27:15 +0200

    blacklist config

--- blacklist/blacklist.py
+++ blacklist/blacklist.py
@@ -12,7 +12,7 @@ class Blacklist(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-        self.config = Config.get_conf(self, identifier=424242)
+        self.config = Config.get_conf(self, identifier=42424269)
         default_guild = {"blacklisted": {}}
         self.config.register_guild(**default_guild)
         self.bsconfig = Config.get_conf(None, identifier=5245652, cog_name="BrawlStarsCog")
