2155783123fed7caf65f04a37de895076f937ced
Anastasia Tarabrina <anastasiya.tarabrina@ru.axxonsoft.com>
2020-07-26 16:57:35 +0300

    achievements cog

--- achievements/achievements.py
+++ achievements/achievements.py
@@ -308,7 +308,7 @@ class Achievements(commands.Cog):
                 if await self.config.user(member).get_raw(keyword):
                     await self.config.user(member).set_raw(keyword, value=False)
                     await ctx.send(embed=goodEmbed(f"Achievement {keyword} was successfully removed from {str(member)}."))
-                if not await self.config.user(member).get_raw(keyword):
+                elif not await self.config.user(member).get_raw(keyword):
                     await self.config.user(member).set_raw(keyword, value=True)
                     await ctx.send(embed=goodEmbed(f"Achievement {keyword} was successfully added to {str(member)}."))
             except Exception as e:
