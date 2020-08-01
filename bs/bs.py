81180a71a794eee05b230ea92abf73f680fba011
Anastasia Tarabrina <anastasiya.tarabrina@ru.axxonsoft.com>
2020-08-01 17:34:33 +0300

    please work

--- bs/bs.py
+++ bs/bs.py
@@ -861,30 +861,30 @@ class BrawlStarsCog(commands.Cog):
                 if h['type'] == "members":
                     name = h['data']['player']['name']
                     tag = "#" + h['data']['player']['tag']
-                    msg += f"☺️ **{name} ({tag}) joined!** {time}\n" if h["data"]["joined"] else f"😱 **{name} ({tag}) left!** {time}\n"
+                    msg += f"➕ **{name} ({tag}) joined!** {time}\n" if h["data"]["joined"] else f"➖ **{name} ({tag}) left!** {time}\n"
                 elif h['type'] == 'settings':
                     if h['data']['type'] == "description":
                         dold = h['data']['old']
                         dnew = h['data']['new']
-                        msg += f"🤔 **Description changed from `{dold}` to `{dnew}`!** {time}\n"
+                        msg += f"**Description changed from `{dold}` to `{dnew}`!** {time}\n"
                     elif h['data']['type'] == "requirement":
                         old = h['data']['old']
                         new = h['data']['new']
-                        msg += f"🤔 **Requirement changed from `{old}` to `{new}`!** {time}\n"
+                        msg += f"**Requirement changed from `{old}` to `{new}`!** {time}\n"
                     elif h['data']['type'] == "status":
                         sold = h['data']['old']
                         snew = h['data']['new']
-                        msg += f"🤔 **Status changed from `{sold}` to `{snew}`!** {time}\n"
+                        msg += f"**Status changed from `{sold}` to `{snew}`!** {time}\n"
                     else:
                         stype = h['data']['type']
                         msg += f"Unrecognized setting type: {stype}\n"
                 elif h['type'] == "roles":
                     if h['data']['promote']:
                         action = "promoted"
-                        emoji = "☺"
+                        emoji = "⬆"
                     else:
                         action = "demoted"
-                        emoji = "😱"
+                        emoji = "⬇"
                     rname = h['data']['player']['name']
                     rtag = "#" + h['data']['player']['tag']
                     rold = h['data']['old']
@@ -900,7 +900,7 @@ class BrawlStarsCog(commands.Cog):
                              discord.Colour.orange(),
                              discord.Colour.red(),
                              discord.Colour.teal()])
-            await ctx.send(len(msg))
+
             await ctx.send(embed=discord.Embed(colour=colour, title=f"{club.name} {club.tag}", description=msg))
 
         else:
