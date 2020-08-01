35dffbd13fafa9ba072cc2f994c45436bad9d1b1
Anastasia Tarabrina <anastasiya.tarabrina@ru.axxonsoft.com>
2020-08-01 18:25:07 +0300

    please work

--- bs/bs.py
+++ bs/bs.py
@@ -861,30 +861,30 @@ class BrawlStarsCog(commands.Cog):
                 if h['type'] == "members":
                     name = h['data']['player']['name']
                     tag = "#" + h['data']['player']['tag']
-                    msg += f"😁 **{name} ({tag}) joined!** {time}\n" if h["data"]["joined"] else f"😔 **{name} ({tag}) left!** {time}\n"
+                    msg += f"<:PepeEnter:730317996127551549> **{name} ({tag}) joined!** {time}\n" if h["data"]["joined"] else f"<:pepeexit:707275210587635812> **{name} ({tag}) left!** {time}\n"
                 elif h['type'] == 'settings':
                     if h['data']['type'] == "description":
                         dold = h['data']['old']
                         dnew = h['data']['new']
-                        msg += f"🤔 **Description changed from `{dold}` to `{dnew}`!** {time}\n"
+                        msg += f"**Description changed from `{dold}` to `{dnew}`!** {time}\n"
                     elif h['data']['type'] == "requirement":
                         old = h['data']['old']
                         new = h['data']['new']
-                        msg += f"🤔 **Requirement changed from `{old}` to `{new}`!** {time}\n"
+                        msg += f"**Requirement changed from `{old}` to `{new}`!** {time}\n"
                     elif h['data']['type'] == "status":
                         sold = h['data']['old']
                         snew = h['data']['new']
-                        msg += f"🤔 **Status changed from `{sold}` to `{snew}`!** {time}\n"
+                        msg += f"**Status changed from `{sold}` to `{snew}`!** {time}\n"
                     else:
                         stype = h['data']['type']
                         msg += f"Unrecognized setting type: {stype}\n"
                 elif h['type'] == "roles":
                     if h['data']['promote']:
                         action = "promoted"
-                        emoji = "😊"
+                        emoji = "<:upvote:554429793446395904>"
                     else:
                         action = "demoted"
-                        emoji = "😬"
+                        emoji = "<:downvote:554429867752685569>"
                     rname = h['data']['player']['name']
                     rtag = "#" + h['data']['player']['tag']
                     rold = h['data']['old']
