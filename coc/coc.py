240c6074ce76f456ec8488217d8a734b6cf8419e
Anastasia Tarabrina <anastasiya.tarabrina@ru.axxonsoft.com>
2020-08-08 15:24:45 +0300

    COC cog

--- coc/coc.py
+++ coc/coc.py
@@ -12,7 +12,7 @@ class ClashOfClansCog(commands.Cog):
         self.config.register_user(**default_user)
         default_guild = {"clans": {}}
         self.config.register_guild(**default_guild)
-        self.cocapi = coc.login("pazuzu636@gmail.com", "Milanisbae")
+        self.cocapi = coc.login("pazuzu636@gmail.com", "Milanisbae", client=coc.Client)
 
     @commands.command()
     async def getCOCname(self, ctx, tag):
