3df4444a687fa4a2388b9da04129fe3c63189f6c
milan_25 <milan.abraham2@seznam.cz>
2020-09-08 18:55:34 +0200

    clans cmd upgrade

--- cr/cr.py
+++ cr/cr.py
@@ -249,148 +249,137 @@ class ClashRoyaleCog(commands.Cog):
             embed.add_field(name="Lowest Members", value=worstm, inline=True)
             return await ctx.send(embed=randomize_colour(embed))  
         
+    @commands.cooldown(1, 5, commands.BucketType.guild)
     @commands.guild_only()
     @commands.group(invoke_without_command=True)
-    async def clans(self, ctx, key:str=None):
+    async def clans(self, ctx, *, keyword: str = ""):
         """View all clans saved in this server"""
         offline = False
-        prefix = ctx.prefix
+        low_clubs = False
+        skip_errors = False
+        reverse_order = False
+        await ctx.trigger_typing()
         await ctx.trigger_typing()
 
-        if key == "forceoffline":
+        if "offline" in keyword:
             offline = True
-            key = None
+            keyword = keyword.replace("offline", "").strip()
 
-        if key is not None and key != "forceoffline":
-            try:
-                if key.startswith("<"):
-                    memberid = key.replace("<", "").replace(">", "").replace("@", "").replace("!", "")
-                    member = discord.utils.get(ctx.guild.members, id=int(memberid))
-                    if member is not None:
-                        mtag = await self.config.user(member).tag()
-                        if mtag is None:
-                            return await ctx.send(embed = self.badEmbed(f"This user has no tag saved! Use {prefix}crsave <tag>"))
+        if "low" in keyword:
+            low_clubs = True
+            keyword = keyword.replace("low", "").strip()
 
-                        try:
-                            player = await self.crapi.get_player(mtag)
-                            tag = player.clan.tag
-                        except clashroyale.RequestError as e:
-                            await ctx.send(embed = self.badEmbed(f"CR API is offline, please try again later! ({str(e)})"))
-                else:
-                    tag = await self.config.guild(ctx.guild).clans.get_raw(key.lower(), "tag", default=None)
-                    if tag is None:
-                        return await ctx.send(embed = self.badEmbed(f"{key.title()} isn't crsaved clan in this server!"))
-                try:
-                    clan = await self.crapi.get_clan(tag)
-                    clan = clan.raw_data
-                
-                except clashroyale.RequestError as e:
-                    await ctx.send(embed = self.badEmbed(f"CR API is offline, please try again later! ({str(e)})"))
-                    return
-                
-                badge = discord.utils.get(self.bot.emojis, name = str(clan['badgeId']))
-                embed=discord.Embed(title=f"{badge}{clan['name']} ({clan['tag']})", description=f"```{clan['description']}```")
-                embed.add_field(name="Members", value=f"<:people:449645181826760734> {clan['members']}/50")
-                embed.add_field(name="Required Trophies", value= f"<:trophycr:587316903001718789> {str(clan['requiredTrophies'])}")
-                embed.add_field(name="Score", value= f"<:crstar:449647025999314954> {str(clan['clanScore'])}")
-                embed.add_field(name="Clan War Trophies", value= f"<:cw_trophy:449640114423988234> {str(clan['clanWarTrophies'])}")
-                embed.add_field(name="Type", value= f"<:bslock:552560387279814690> {clan['type'].title()}".replace("only", " Only"))
-                embed.add_field(name="Location", value=f":earth_africa: {clan['location']['name']}")
-                embed.add_field(name="Average Donations Per Week", value= f"<:deck:451062749565550602> {str(clan['donationsPerWeek'])}")
-                return await ctx.send(embed=randomize_colour(embed))            
-                
-            except Exception as e:
-                return await ctx.send("**Something went wrong, please send a personal message to LA Modmail bot or try again!**")
-        
+        if "skiperrors" in keyword:
+            skip_errors = True
+            keyword = keyword.replace("skiperrors", "").strip()
+
+        if "reverse" in keyword:
+            reverse_order = True
+            keyword = keyword.replace("reverse", "").strip()
+
         if len((await self.config.guild(ctx.guild).clans()).keys()) < 1:
             return await ctx.send(embed = self.badEmbed(f"This server has no clans saved. Save a clan by using {ctx.prefix}clans add!"))
-                                
-        try:
-            try:
-                clans = []
-                for key in (await self.config.guild(ctx.guild).clans()).keys():
-                    clan = await self.crapi.get_clan(await self.config.guild(ctx.guild).clans.get_raw(key, "tag"))
-                    clans.append(clan.raw_data)
-            except clashroyale.RequestError as e:
-                offline = True
-            
-            embedFields = []
-            
-            if not offline:
-                clans = sorted(clans, key=lambda sort: (sort['requiredTrophies'], sort['clanScore']), reverse=True)
-                
-                for i in range(len(clans)):   
-                    cemoji = discord.utils.get(self.bot.emojis, name = str(clans[i]['badgeId']))
-                    key = ""
-                    for k in (await self.config.guild(ctx.guild).clans()).keys():
-                        if clans[i]['tag'].replace("#", "") == await self.config.guild(ctx.guild).clans.get_raw(k, "tag"):
-                            key = k
-                    
-                    await self.config.guild(ctx.guild).clans.set_raw(key, 'lastMemberCount', value=clans[i]['members'])            
-                    await self.config.guild(ctx.guild).clans.set_raw(key, 'lastRequirement', value=clans[i]['requiredTrophies'])   
-                    await self.config.guild(ctx.guild).clans.set_raw(key, 'lastScore', value=clans[i]['clanScore'])               
-                    await self.config.guild(ctx.guild).clans.set_raw(key, 'lastPosition', value=i)               
-                    await self.config.guild(ctx.guild).clans.set_raw(key, 'lastBadgeId', value=clans[i]['badgeId'])   
-                    await self.config.guild(ctx.guild).clans.set_raw(key, 'warTrophies', value=clans[i]['clanWarTrophies'])   
-                   
-                    info = await self.config.guild(ctx.guild).clans.get_raw(key, "info", default="")
-                    e_name = f"{str(cemoji)} {clans[i]['name']} [{key}] ({clans[i]['tag']}) {info}"
-                    e_value = f"<:people:449645181826760734>`{clans[i]['members']}` <:trophycr:587316903001718789>`{clans[i]['requiredTrophies']}+` <:crstar:449647025999314954>`{clans[i]['clanScore']}` <:cw_trophy:449640114423988234>`{clans[i]['clanWarTrophies']}`"
-                    embedFields.append([e_name, e_value])
-            
-            else:
-                offclans = []
-                for k in (await self.config.guild(ctx.guild).clans()).keys():
-                    offclans.append([await self.config.guild(ctx.guild).clans.get_raw(k, "lastPosition"), k])
-                offclans = sorted(offclans, key=lambda x: x[0])
-                                
-                for clan in offclans:
-                    ckey = clan[1]
-                    cscore = await self.config.guild(ctx.guild).clans.get_raw(ckey, "lastScore")
-                    cname = await self.config.guild(ctx.guild).clans.get_raw(ckey, "name")
-                    ctag = await self.config.guild(ctx.guild).clans.get_raw(ckey, "tag")
-                    cinfo = await self.config.guild(ctx.guild).clans.get_raw(ckey, "info")
-                    cmembers = await self.config.guild(ctx.guild).clans.get_raw(ckey, "lastMemberCount")
-                    creq = await self.config.guild(ctx.guild).clans.get_raw(ckey, "lastRequirement")
-                    ccw = await self.config.guild(ctx.guild).clans.get_raw(ckey, "warTrophies")        
-                    cemoji = discord.utils.get(self.bot.emojis, name = str(await self.config.guild(ctx.guild).clans.get_raw(ckey, "lastBadgeId")))
-                    
-                    e_name = f"{cemoji} {cname} [{ckey}] (#{ctag}) {cinfo}"
-                    e_value = f"<:people:449645181826760734>`{cmembers}` <:trophycr:587316903001718789>`{creq}+` <:crstar:449647025999314954>`{cscore}` <:cw_trophy:449640114423988234>`{ccw}`"
-                    embedFields.append([e_name, e_value])
-            
-            colour = choice([discord.Colour.green(), discord.Colour.blue(), discord.Colour.purple(), discord.Colour.orange(), discord.Colour.red(), discord.Colour.teal()])
-            embedsToSend = []                
-            for i in range(0, len(embedFields), 8):
-                embed = discord.Embed(colour=colour)
-                embed.set_author(name=f"{ctx.guild.name} clans", icon_url=ctx.guild.icon_url)
-                footer = "API is offline, showing last saved data." if offline else f"Do you need more info about a clan? Use {ctx.prefix}clan [key]"
-                embed.set_footer(text = footer)
-                for e in embedFields[i:i+8]:
-                    embed.add_field(name=e[0], value=e[1], inline=False)
-                embedsToSend.append(embed)
-             
-            async def next_page(ctx: commands.Context, pages: list, controls: dict, message: discord.Message, page: int, timeout: float, emoji: str):
-                perms = message.channel.permissions_for(ctx.me)
-                if perms.manage_messages:
-                    try:
-                        await message.remove_reaction(emoji, ctx.author)
-                    except discord.NotFound:
-                        pass
-                if page == len(pages) - 1:
-                    page = 0
-                else:
-                    page = page + 1
-                return await menu(ctx, pages, controls, message=message, page=page, timeout=timeout)                  
-            if len(embedsToSend) > 1:                   
-                await menu(ctx, embedsToSend, {"➡": next_page} , timeout=300)
-            else:
-                await ctx.send(embed=embedsToSend[0])
-                                
-        except Exception as e:
-            return await ctx.send("**Something went wrong, please send a personal message to LA Modmail bot or try again!**")
-                                
-                                
+                            
+        saved_clans = await self.config.guild(ctx.guild).clans()
+                            
+        try:    
+            clans = []
+            for key in saved_clans.keys():
+                if offline:
+                    break
+                try:
+                    clan = await self.crapi.get_clan(daved_clans[key, "tag"])
+                except clashroyale.RequestError as e:
+                    if skip_errors:
+                        continue
+                    else:
+                        offline = True
+                        break
+                clans.append(clan.raw_data)
+
+        embedFields = []
+
+        if not offline:
+            clans = sorted(clans, key=lambda sort: (sort['requiredTrophies'], sort['clanScore']), reverse=not reverse_order)
+
+            for i in range(len(clans)):   
+                key = ""
+                for k in saved_clans.keys():
+                    if clans[i]['tag'].replace("#", "") == saved_clans[k, "tag"]:
+                        key = k
+                            
+                cemoji = discord.utils.get(self.bot.emojis, name = str(clans[i]['badgeId']))
+                            
+                saved_clans[key]['lastMemberCount'] = clans[i]['members']
+                saved_clans[key]['lastRequirement'] = clans[i]['requiredTrophies']
+                saved_clans[key]['lastScore'] = clans[i]['clanScore']
+                saved_clans[key]['lastPosition'] = i
+                saved_clans[key]['lastBadgeId'] = clans[i]['badgeId']
+                saved_clans[key]['warTrophies'] = clans[i]['clanWarTrophies']
+                            
+                info = saved_clans[key]['info'] if "info" in saved_clans[key] else ""
+                            
+                if low_clubs and len(clubs[i].members) >= 45:
+                    continue
+
+                e_name = f"{str(cemoji)} {clans[i]['name']} [{key}] ({clans[i]['tag']}) {info}"
+                e_value = f"<:people:449645181826760734>`{clans[i]['members']}` <:trophycr:587316903001718789>`{clans[i]['requiredTrophies']}+` <:crstar:449647025999314954>`{clans[i]['clanScore']}` <:cw_trophy:449640114423988234>`{clans[i]['clanWarTrophies']}`"
+                embedFields.append([e_name, e_value])
+                            
+            await self.config.guild(ctx.guild).set_raw("clans", value=saved_clans)
+
+        else:
+            offclans = []
+            for k in saved_clans.keys():
+                offclans.append([saved_clans[k]['lastPosition'], k])
+            offclans = sorted(offclans, key=lambda x: x[0], reverse=reverse_order)
+
+            for clan in offclans:
+                ckey = clan[1]
+                cscore = saved_clans[ckey]["lastScore"]
+                cname = saved_clans[ckey]["name"]
+                ctag = saved_clans[ckey]["tag"]
+                cinfo = saved_clans[ckey]["info"]
+                cmembers = saved_clans[ckey]["lastMemberCount"]
+                creq = saved_clans[ckey]["lastRequirement"]
+                ccw = saved_clans[ckey]["warTrophies"]
+                             
+                cemoji = discord.utils.get(self.bot.emojis, name = str(saved_clans[ckey]["lastBadgeId"]))
+                            
+                if low_clubs and cmembers >= 45:
+                    continue
+
+                e_name = f"{cemoji} {cname} [{ckey}] (#{ctag}) {cinfo}"
+                e_value = f"<:people:449645181826760734>`{cmembers}` <:trophycr:587316903001718789>`{creq}+` <:crstar:449647025999314954>`{cscore}` <:cw_trophy:449640114423988234>`{ccw}`"
+                embedFields.append([e_name, e_value])
+
+        colour = choice([discord.Colour.green(),
+                         discord.Colour.blue(),
+                         discord.Colour.purple(),
+                         discord.Colour.orange(),
+                         discord.Colour.red(),
+                         discord.Colour.teal()])
+        embedsToSend = []                
+        for i in range(0, len(embedFields), 8):
+            embed = discord.Embed(colour=colour)
+            embed.set_author(name=f"{ctx.guild.name} clans", icon_url=ctx.guild.icon_url)
+            page = (i // 8) + 1
+            footer =f"[{page}/{len(embedFields)//8+1}] API is offline, showing last saved data." if offline else f"[{page}/{len(embedFields)//8+1}] Do you need more info about a clan? Use {ctx.prefix}clan [key]"
+            embed.set_footer(text = footer)
+            for e in embedFields[i:i+8]:
+                embed.add_field(name=e[0], value=e[1], inline=False)
+            embedsToSend.append(embed)
+
+        if len(embedsToSend) > 1:
+            await msg.delete()
+            await menu(ctx, embedsToSend, {"⬅": prev_page, "➡": next_page, }, timeout=2000)
+        elif len(embedsToSend) == 1:
+            await msg.delete()
+            await ctx.send(embed=embedsToSend[0])
+        else:
+            await msg.delete()
+            await ctx.send("No clans found!")
+          
     @commands.guild_only()
     @commands.has_permissions(administrator = True) 
     @clans.command(name="add")
