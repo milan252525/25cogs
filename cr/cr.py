5cd65ebe5945ff958c9310d0d8c1b44e5d8e6109
milan_25 <milan.abraham2@seznam.cz>
2020-09-08 19:13:35 +0200

    clans cmd upgrade

--- cr/cr.py
+++ cr/cr.py
@@ -1,7 +1,7 @@
 import discord
 from redbot.core import commands, Config, checks
 from redbot.core.utils.embed import randomize_colour
-from redbot.core.utils.menus import menu, DEFAULT_CONTROLS
+from redbot.core.utils.menus import menu, prev_page, next_page
 from random import choice
 import clashroyale
 from typing import Union
