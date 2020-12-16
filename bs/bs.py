1a547e34d25526c51465642a21d3bb2137e8430a
milan252525 <milan.abraham2@seznam.cz>
2020-12-16 18:43:01 +0100

    make it easier to find active maps

--- bs/bs.py
+++ bs/bs.py
@@ -616,6 +616,7 @@ class BrawlStarsCog(commands.Cog):
             final = {}
             all_maps = await self.starlist_request("https://api.starlist.pro/maps")
             for m in all_maps['list']:
+                hash_ = m['hash'] + "-old" if m['disabled'] else m['hash']
                 final[m['hash']] = {'url': m['imageUrl'], 'name': m['name'], 
                                    'disabled': m['disabled'], 'link': m['link'],
                                    'gm_url': m['gameMode']['imageUrl'], 'id': m['id']}
