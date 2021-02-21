ac46df0283232e3ff36925ee7fa11a98eca04ebe
milan252525 <milan.abraham2@seznam.cz>
2021-02-21 19:51:14 +0100

    formatting

--- bs/bs.py
+++ bs/bs.py
@@ -780,7 +780,7 @@ class BrawlStarsCog(commands.Cog):
         country = pycountry.countries.get(alpha_2=region)
         name = region[:30].upper() if country is None else country.name.upper()
         for club in lb:
-            cname = club['name'].replace('*', '\\*').replace('_', '\\_')
+            cname = club['name'].replace('*', '\*').replace('_', '\_')
             line = f"`{club['rank']:03d}` **{cname}** {club['tag']} `{club['trophies']}` {club['member_count']}\n"
             if len(result) + len(line) > 2000:
                 embeds.append(discord.Embed(colour=discord.Color.random(), description=result, title=name))
