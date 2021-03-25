51f441430fea281c91ec2984783d51bfc8370573
milan252525 <milan.abraham2@seznam.cz>
2021-03-25 14:47:05 +0100

    slash profile test

--- slash/__init__.py
+++ slash/__init__.py
@@ -3,3 +3,4 @@ from .slash import Slash
 async def setup(bot):
   cog = Slash(bot)
   bot.add_cog(cog)
+  await bot.slash.sync_all_commands()
--- slash/slash.py
+++ slash/slash.py
@@ -2,26 +2,31 @@ import discord
 from redbot.core import commands
 from discord_slash import SlashCommand
 from discord_slash import cog_ext, SlashContext
-import logging
+
+from typing import Union
 
 class Slash(commands.Cog):
     def __init__(self, bot):
         if not hasattr(bot, "slash"):
-            logging.error("condition triggered")
             bot.slash = SlashCommand(bot, sync_commands=True)
         self.bot = bot
-
-
-    @cog_ext.cog_slash(name="test")
-    async def _test(self, ctx: SlashContext):
-        embed = discord.Embed(title="embed test")
-        await ctx.send(content="test", embeds=[embed])
 
     @cog_ext.cog_slash(
         name="slashtest", 
         description="test command, might explode",
         guild_ids=[401883208511389716]
     )
-    async def slash_test(self, ctx: SlashContext):
+    async def slash_test(self, ctx: SlashContext, member):
         await ctx.respond()
         await ctx.send(content="slash works")
+
+    @cog_ext.cog_slash(
+        name="ptest", 
+        description="Get your BS stats",
+        guild_ids=[401883208511389716]
+    )
+    async def p_test(self, ctx: SlashContext, member: Union[discord.Member, str] = None):
+        await ctx.respond()
+        await ctx.send(content=str(member))
+
+        
