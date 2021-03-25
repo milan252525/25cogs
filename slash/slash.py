352feedb20581ea2d5116e09a96ff26fdf431614
milan252525 <milan.abraham2@seznam.cz>
2021-03-25 15:04:17 +0100

    slash remove test

--- slash/slash.py
+++ slash/slash.py
@@ -2,14 +2,18 @@ import discord
 from redbot.core import commands
 from discord_slash import SlashCommand
 from discord_slash import cog_ext, SlashContext
+from discord_slash.utils.manage_commands import remove_all_commands
 
 from typing import Union
 
 class Slash(commands.Cog):
     def __init__(self, bot):
         if not hasattr(bot, "slash"):
-            bot.slash = SlashCommand(bot, sync_commands=True)
+            bot.slash = SlashCommand(bot, sync_on_cog_reload=True)
         self.bot = bot
+
+    def cog_unload(self):
+        self.bot.slash.remove_cog_commands(self)
 
     @cog_ext.cog_slash(
         name="slashtest", 
