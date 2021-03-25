16b1a1487cffbcd730a7cc4525533af83395937e
milan252525 <milan.abraham2@seznam.cz>
2021-03-25 14:13:59 +0100

    slash test

--- slash/slash.py
+++ slash/slash.py
@@ -11,11 +11,17 @@ class Slash(commands.Cog):
             bot.slash = SlashCommand(bot, sync_commands=True)
         self.bot = bot
 
+
     @cog_ext.cog_slash(name="test")
     async def _test(self, ctx: SlashContext):
         embed = discord.Embed(title="embed test")
         await ctx.send(content="test", embeds=[embed])
 
-    @cog_ext.cog_slash(name="slashtest")
+    @cog_ext.cog_slash(
+        name="slashtest", 
+        description="test command, might explode",
+        guild_ids=[401883208511389716]
+    )
     async def slash_test(self, ctx: SlashContext):
+        await ctx.respond()
         await ctx.send(content="slash works")
