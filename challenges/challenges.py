ebdf47955fc572df092cbb2fa74bb2b91a1829d2
milan252525 <milan.abraham2@seznam.cz>
2021-04-10 20:46:01 +0200

    progress loop

--- challenges/challenges.py
+++ challenges/challenges.py
@@ -16,7 +16,7 @@ class Challenges(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.config = Config.get_conf(self, identifier=25202025)
-        default_member = {"tokens" : 0, "progress" : {}, }
+        default_member = {"tokens" : 0}
         default_server = {"active_challenges" : {}, "channel" : None, "log": None}
         self.config.register_member(**default_member)
         self.config.register_global(**default_server)
@@ -30,9 +30,11 @@ class Challenges(commands.Cog):
             "full": ["<:loadleft:821065351726366761>", "<:loadmid:821065352061780048>", "<:loadright:821065351903182889>"]
         }
         self.challenge_update_loop.start()
+        self.progress_update_loop.start()
 
     def cog_unload(self):
         self.challenge_update_loop.stop()
+        self.progress_update_loop.stop()
 
     async def initialize(self):
         ofcbsapikey = await self.bot.get_shared_api_tokens("ofcbsapi")
@@ -75,8 +77,8 @@ class Challenges(commands.Cog):
         #prepare challenges 24h in advance
         for chal_id in self.challenge_data:
             start_date = dt.strptime(self.challenge_data[chal_id]["start"], "%d/%m/%y %H:%M")
-            #end_date = dt.strptime(self.challenge_data[chal_id]["end"], "%d/%m/%y %H:%M")
-            if chal_id not in labs_challs.keys() and ((start_date - now) < timedelta(hours=24)):
+            end_date = dt.strptime(self.challenge_data[chal_id]["end"], "%d/%m/%y %H:%M")
+            if chal_id not in labs_challs.keys() and ((start_date - now) < timedelta(hours=24)) and (now < end_date):
                 await self.log(log_channel, f"Setting up upcoming {chal_id}", discord.Color.orange())
                 data = self.challenge_data[chal_id].copy()
                 data["status"] = "start_soon"
@@ -88,7 +90,7 @@ class Challenges(commands.Cog):
                 data["progress"] = 0
                 labs_challs[chal_id] = data
                 await self.config.guild(labs).set_raw("active_challenges", chal_id, value=data)
-        #update prepared and start
+        #update prepared and handle status changes
         for chal_id in labs_challs:
             data = labs_challs[chal_id]
             if data["status"] == "start_soon":
@@ -106,16 +108,18 @@ class Challenges(commands.Cog):
                     message = labs_channel.get_partial_message(data["message_id"])
                     embed = discord.Embed.from_dict(data["embed"])
                     embed.set_field_at(0, name="Time left", value=ends)
+                    embed.description = embed.description + "\nCheck your progress with `/ch stats`"
                     if 'global' in data:
                         glob_pro= f"0/{data['global']['goal']}\n" + self.loading['empty'][0] + self.loading['empty'][1]*8 + self.loading['empty'][2]
                         embed.add_field(name="Progress", value=glob_pro, inline=False)
-                    participants = len(data['participants'].keys())
-                    embed.set_footer(text="Participants: " + str(participants))
+                    participants = data["participants"]
+                    embed.set_footer(text="Participants: " + str(len(participants.keys())))
                     data["embed"] = embed.to_dict()
                     await message.edit(embed=embed)
-                    await self.config.guild(labs).set_raw("active_challenges", chal_id, value=data)
+                    await self.config.guild(labs).set_raw("active_challenges", chal_id, "status", value="active")
+                    await self.config.guild(labs).set_raw("active_challenges", chal_id, "embed", value=embed.to_dict())
             if data["status"] == "active":
-                await self.challenge_update() #update scores
+                #score update is independent loop
 
                 end_date = dt.strptime(data["end"], "%d/%m/%y %H:%M")
                 if now >= end_date:
@@ -125,13 +129,31 @@ class Challenges(commands.Cog):
                     embed = discord.Embed.from_dict(data["embed"])
                     embed.set_field_at(0, name="Time left", value="Ending soon...")
                     await message.edit(embed=embed)
-                    await self.config.guild(labs).set_raw("active_challenges", chal_id, value=data)
+                    await self.config.guild(labs).set_raw("active_challenges", chal_id, "status", value="to_be_ended")
                 else:
                     message = labs_channel.get_partial_message(data["message_id"])
                     embed = discord.Embed.from_dict(data["embed"])
                     ends = self.time_left((end_date - now).total_seconds())
                     embed.set_field_at(0, name="Time left", value=ends)
+
+                    participants = data["participants"]
+                    if 'global' in data:
+                        total = sum([participants["progress"] for id in participants])
+                        percentage = int((total/data['global']['goal'])*100)
+                        first = self.loading['empty'][0] if percentage < 10 else self.loading['full'][0]
+                        full = int((percentage-10)/10)
+                        if full < 0:
+                            full = 0
+                        if full > 8:
+                            full = 8
+                        middle = self.loading['full'][1] * full + self.loading['empty'][1] * (8 - full)
+                        last = self.loading['empty'][2] if percentage < 100 else self.loading['full'][2]
+                        glob_pro = f"{total}/{data['global']['goal']}\n" + first + middle + last
+                        embed.set_field_at(2, name="Progress", value=glob_pro, inline=False)
+                    embed.set_footer(text="Participants: " + str(len(participants.keys())))
                     await message.edit(embed=embed)
+                    data["embed"] = embed.to_dict()
+                    await self.config.guild(labs).set_raw("active_challenges", chal_id, "embed", value=embed.to_dict())
 
             if data["status"] == "to_be_ended":
                 await self.log(log_channel, f"Finishing {chal_id}", discord.Color.purple())
@@ -143,9 +165,11 @@ class Challenges(commands.Cog):
                 embed = discord.Embed.from_dict(data["embed"])
                 embed.remove_field(0)
                 embed.title = "[ENDED] " + data["name"]
+                embed.description = data["description"] + "\nCheck your progress with `/ch stats`"
                 data["embed"] = embed.to_dict()
                 await message.edit(embed=embed)
-                await self.config.guild(labs).set_raw("active_challenges", chal_id, value=data)
+                await self.config.guild(labs).set_raw("active_challenges", chal_id, "status", value="ended")
+                await self.config.guild(labs).set_raw("active_challenges", chal_id, "embed", value=embed.to_dict())
 
             if data["status"] == "ended":
                 end_date = dt.strptime(data["end"], "%d/%m/%y %H:%M")
@@ -157,6 +181,110 @@ class Challenges(commands.Cog):
         
     @challenge_update_loop.error
     async def challenge_update_loop_errors(self, error):
+        await self.log_error(error)
+
+    @tasks.loop(minutes=3)
+    async def progress_update_loop(self):
+        labs = self.bot.get_guild(self.labs)
+        labs_channel_id = await self.config.guild(labs).channel()
+        labs_log_id = await self.config.guild(labs).log()
+        if labs_channel_id is None or labs_log_id is None:
+            return
+        labs_channel = labs.get_channel(labs_channel_id)
+        log_channel = labs.get_channel(labs_log_id)
+        labs_challs = await self.config.guild(labs).active_challenges()
+        if labs_challs is None:
+            await self.config.guild(labs).set_raw("active_challenges", value={})
+            labs_challs = {}
+        now = dt.now()
+        bs_conf = self.get_bs_config()
+        tags = await bs_conf.all_users()
+        
+        for chall_id in labs_challs:
+            if labs_challs[chall_id]["status"] == "active":
+                data = labs_challs[chall_id]
+                participants = data["participants"]
+                gamemodes = data["gamemodes"]
+                brawlers = data["brawlers"]
+                type = data["type"]
+                min_trophy = data["min_trophy"]
+
+                score = 0
+
+                for mem_id in participants:
+                    last = participants[mem_id]["last"]
+                    user = labs.get_member(mem_id)
+                    if user is None:
+                        continue
+                    tag = tags[user.id]['tag'].replace("o", "0").replace("O", "0")
+                    try:
+                        log = await self.ofcbsapi.get_battle_logs(tag)
+                        await asyncio.sleep(0.03)
+                        log = log.raw_data
+                    except brawlstats.errors.RequestError as e:
+                        await self.log_error(e)
+                        break
+                    except Exception as e:
+                        await self.log_error(e)
+                        break
+                    for battle in log:
+                        b_time = dt.strptime(battle['battleTime'], '%Y%m%dT%H%M%S.%fZ')
+                        if b_time <= dt.strptime(last, '%Y%m%dT%H%M%S.%fZ'):
+                            break
+                        
+                        if "id" in battle['event'] and battle['event']['id'] == 0 and ("type" not in battle['battle'] or battle['battle']['type'] != "ranked"):
+                            continue
+                        if "type" in battle['battle'] and battle['battle']['type'] == "friendly":
+                            continue
+                        
+                        if len(gamemodes) > 0 and battle['battle']['mode'] not in gamemodes:
+                            continue
+
+                        win = True
+                        if "result" in battle['battle'] and battle['battle']['result'] != "victory":
+                            win = False
+                        if "rank" in battle['battle'] and battle['battle']['mode'] == "soloShowdown" and battle['battle']['rank'] > 4:
+                            win = False
+                        if "rank" in battle['battle'] and battle['battle']['mode'] != "soloShowdown" and battle['battle']['rank'] > 2:
+                            win = False
+
+                        if type == "wins" and not win:
+                            return
+
+                        player = None
+                        if "teams" in battle['battle']:
+                            for t in battle['battle']['teams']:
+                                for p in t:
+                                    if p['tag'].replace("#", "") == tag.upper():
+                                        player = p
+                        else:
+                            for p in battle['battle']['players']:
+                                if p['tag'].replace("#", "") == tag.upper():
+                                    player = p
+                        if player is None:
+                            await self.log_error(e)
+                            continue
+
+                        if "trophies" not in player['brawler']:
+                            continue
+                        if player['brawler']['trophies'] < min_trophy:
+                            continue
+
+                        brawler_name = player['brawler']['name']
+                        if len(brawlers) > 1 and brawler_name not in brawlers:
+                            return
+
+                        score += 1
+                    old = participants[mem_id]["progress"]
+                    await self.config.guild(labs).set_raw("active_challenges", chall_id, "participants", str(mem_id), value={"progress": old + score, "last": log[0]['battleTime']})
+                    if score != 0:
+                        await self.log(log_channel, f"[{chall_id}] {user.display_name} +{score}")
+
+    @progress_update_loop.error
+    async def progress_update_loop_errors(self, error):
+        await self.log_error(error)
+
+    async def log_error(self, error):
         traceback.print_exc()
         labs = self.bot.get_guild(self.labs)
         labs_log_id = await self.config.guild(labs).log()
@@ -175,6 +303,7 @@ class Challenges(commands.Cog):
             )
         await log_channel.send(embed=embed)
 
+    
     async def log(self, channel, message, colour=discord.Colour.green()):
         embed=discord.Embed(colour=colour, description=message)
         await channel.send(embed=embed)
@@ -190,7 +319,7 @@ class Challenges(commands.Cog):
         #ends = self.time_left((dt.strptime(data["end"], "%d/%m/%y %H:%M") - dt.now()).total_seconds())
         embed = discord.Embed(
             title = data["name"],
-            description=data["description"] + f"\n\nJoin with `/ch join {chal_id}`\nCheck your progress with `/ch stats`",
+            description=data["description"] + f"\n\nJoin with `/ch join {chal_id}`",
             colour=discord.Colour.random()
         )
         embed.add_field(name="Starts in", value=starts, inline=False)
@@ -242,7 +371,35 @@ class Challenges(commands.Cog):
         challs = await self.config.guild(ctx.guild).active_challenges()
         if challenge_id not in challs.keys():
             return await ctx.send(f"No challenge with ID {challenge_id} found!")
+        bs_conf = self.get_bs_config()
+        if (await bs_conf.user(ctx.author).tag()) is None:
+            return await ctx.send("Save your tag using `/save` first!")
         if ctx.author.id in challs[challenge_id]["participants"]:
             return await ctx.send(f"You are already participating in {challs[challenge_id]['name']}.")
-        await self.config.guild(ctx.guild).set_raw("active_challenges", challenge_id, "participants", str(ctx.author.id), value=0)
+        time = (dt.strptime(challs[challenge_id]["start"], "%d/%m/%y %H:%M")).strftime("%Y%m%dT%H%M%S.%fZ")
+        await self.config.guild(ctx.guild).set_raw("active_challenges", challenge_id, "participants", str(ctx.author.id), value={"progress": 0, "last": time})
         return await ctx.send(f"{ctx.author.mention} sucessfully joined {challs[challenge_id]['name']}!")
+
+    @commands.guild_only()
+    @challenge.command(name="stats")
+    async def challenge_stats(self, ctx, target: discord.User = None):
+        if ctx.guild.id != self.labs:
+            return
+        if target is None:
+            target = ctx.author
+        embed = discord.Embed(colour=discord.Colour.green())
+        embed.set_author(name=f"{target.display_name}'s challenge stats", icon_url=target.avatar_url)
+        embed.add_field(name="Tokens", value=await self.config.member(target).tokens(), inline=False)
+        progress = ""
+        challs = await self.config.guild(ctx.guild).active_challenges()
+        for chall_id in challs:
+            if target.id in challs[chall_id]["participants"]:
+                if challs[chall_id]['status'] == "start_soon":
+                    progress += f"**{challs[chall_id]['name']}** `hasn't started yet`\n"
+                else:
+                    progress += f"**{challs[chall_id]['name']}** `{challs[chall_id]['participants'][target.id]['progress']}`\n"
+            else:
+                progress += f"**{challs[chall_id]['name']}** `not participating`\n"
+        embed.add_field(name="Progress", value=progress, inline=False)
+        await ctx.send(embed=embed)
+        
