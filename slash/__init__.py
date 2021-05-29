72d654b73ccb19cdcf12036f2459a34b9242cdb2
milan_25 <milan.abraham2@seznam.cz>
2021-05-29 18:57:37 +0200

    initialize slash client before cog loads

--- slash/__init__.py
+++ slash/__init__.py
@@ -1,6 +1,9 @@
 from .slash import Slash
+from discord_slash import SlashCommand
 
 async def setup(bot):
+  if not hasattr(bot, "slash"):
+    bot.slash = SlashCommand(bot, sync_on_cog_reload=True)
   cog = Slash(bot)
   bot.add_cog(cog)
   await bot.slash.sync_all_commands()
